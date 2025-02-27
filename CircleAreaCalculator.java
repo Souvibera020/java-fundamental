26f6a035129258beb3abbb1638befa98b9d1d906
Souvik Bera <souvikbera020@gmail.com>
2025-02-27 13:10:49 +0530

    CircleAreaCalculator

---
+++ CircleAreaCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CircleAreaCalculator {
+
+    public static double calculateArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+        double area = calculateArea(radius);
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+        scanner.close();
+    }
+}
